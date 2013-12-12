d3073c4cecde2af596355885f94dfafc928149d9
ningsuhen <ningsuhen@gmail.com>
2013-12-12 21:18:15 +0530

    unknown changes

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express')
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-var lessMiddleware = require('less-middleware');
-var jst = require('jst');
-jst.compiler = 'underscore';
-jst.compile(__dirname + '/public/tpl', __dirname + '/public/js', function() {
-  return console.log('recompiled to /public/js/templates.js');
-});
-
 var app = express();
+
+// development only
+if ('development' == app.get('env')) {
+  // compile LESS files to css
+  app.use(require('less-middleware')({
+    src : __dirname + '/vendor/twitter/bootstrap/less',
+    dest : __dirname + '/public',
+    prefix : '/css',
+    compress : true
+  }));
+  // compile underscore templates to single JST File
+  var jst = require('jst');
+  jst.compiler = 'underscore';
+  jst.compile(__dirname + '/public/tpl', __dirname + '/public/js', function() {
+    return console.log('recompiled to /public/js/templates.js');
+  });
+}
+app.use(express.static(path.join(__dirname, 'public')));
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -27,44 +41,14 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
-app.use(require('less-middleware')({
-  src : __dirname + '/vendor/twitter/bootstrap/less',
-  dest : __dirname + '/public',
-  prefix : '/css',
-  compress : true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// production only
+// if ('production' == app.get('env')) {
+// app.set('db uri', 'n.n.n.n/prod');
+// }
 
 // app.get('/', routes.index);
-// app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-// app.configure('development', function () {
-// app.use(express.errorHandler({
-// dumpExceptions : true,
-// showStack : true
-// }));
-// });
-
-// app.configure('production', function () {
-// app.use(express.errorHandler());
-// });
-
-// Compatible
-
-// Routes
-
-// app.get('/', routes.index);
-//
-// app.listen(3000, function () {
-// console.log("Express server listening on port %d in %s mode",
-// app.address().port, app.settings.env);
-// });
